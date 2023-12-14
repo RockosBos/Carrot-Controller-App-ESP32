15f4ea00860861f38dd74680f6ea1c13b22a726a
RockosBos <32206608+RockosBos@users.noreply.github.com>
2023-12-14 00:24:00 -0500

    Communication working

--- esp32ReactApp/App.tsx
+++ esp32ReactApp/App.tsx
@@ -33,7 +33,7 @@ let devices;
 const buffer = Buffer.from([0, 0]);
 let serviceUUID, charUUID;
 const manager = new BleManager();
-const value = base64.encode("Hello");
+const value = "1001";
 
 
 function App(): JSX.Element {
@@ -43,6 +43,7 @@ function App(): JSX.Element {
 		let serviceUUID = "", charUUID = "";
 		console.log("Scanning Devices");
 		manager.startDeviceScan(null, null, (error, device) => {
+			//console.log(device?.name);
 			if(device?.name == "MyESP32"){
 				console.log("ESP Device Found");
 				manager.stopDeviceScan();
@@ -72,7 +73,10 @@ function App(): JSX.Element {
 										console.log("Retrieved Id's!");
 										console.log("ServiceID: " + serviceUUID + " | CharacteristicID: " + charUUID);
 
-										manager.writeCharacteristicWithoutResponseForDevice(device.id, serviceUUID, charUUID, value);
+										manager.writeCharacteristicWithResponseForDevice(device.id, serviceUUID, charUUID, base64.encode(value),
+										).then(characteristic => {
+										  	console.log('Boxvalue changed to :', base64.decode(characteristic.value));
+										});
 									});
 								}
 
